44fbac11d8ab98e156abf327350277e8ce7275cb
Iuri Ferreira <bokin@me.com>
2023-06-20 17:44:23 -0300

    Added login view

---
+++ Dinero/Login/LoginView.swift
@@ -0,0 +1,53 @@
+//
+//  LoginView.swift
+//  Dinero
+//
+//  Created by Iuri Ferreira on 20/06/23.
+//
+
+import Foundation
+import UIKit
+
+class LoginView : UIView {
+    
+    let loginTextField = UITextField()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        style()
+        layout()
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: 200, height: 200)
+    }
+    
+}
+
+extension LoginView {
+    func style() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        loginTextField.translatesAutoresizingMaskIntoConstraints = false
+        loginTextField.placeholder = "Digite seu texto aqui"
+        backgroundColor = .gray
+    }
+    
+    func layout() {
+        addSubview(loginTextField)
+       
+        
+        NSLayoutConstraint.activate([
+            loginTextField.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor, constant: 1),
+            loginTextField.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant: 1),
+            loginTextField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -1)
+        ])
+
+    }
+}
---
+++ Dinero/Login/LoginViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Dinero
+//
+//  Created by Iuri Ferreira on 20/06/23.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    let loginView = LoginView()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        style()
+        layout()
+    }
+
+}
+
+
+extension LoginViewController {
+    private func style() {
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+    private func layout() {
+        view.addSubview(loginView)
+        
+        NSLayoutConstraint.activate([
+            loginView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            loginView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: loginView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
+
