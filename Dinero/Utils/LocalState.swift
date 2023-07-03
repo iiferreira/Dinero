df324c36955afdf70e942b06d79ac28ab79ca648
Iuri Ferreira <bokin@me.com>
2023-07-03 08:50:26 -0300

    Create localstate to track app flow

--- Dinero/Utils/LocalState.swift
+++ Dinero/Utils/LocalState.swift
@@ -20,7 +20,6 @@ public class LocalState {
         }
         set (newValue) {
             UserDefaults.standard.set(newValue, forKey: Keys.hasOnboarded.rawValue)
-            UserDefaults.standard.synchronize()
         }
     }
 }
