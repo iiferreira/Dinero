6df5b9cc6be1e8039de433629b5149ba74bd64e4
Iuri Ferreira <bokin@me.com>
2023-07-31 17:49:20 -0300

    Added shakey bell

--- Dinero/AccountSummary/Header/AccountSummaryHeaderView.swift
+++ Dinero/AccountSummary/Header/AccountSummaryHeaderView.swift
@@ -11,31 +11,49 @@ class AccountSummaryHeaderView : UIView {
     
     @IBOutlet var contentView: UIView!
     
+    let ShakeyBell = ShakeyBellView()
+    
+    
     override init(frame: CGRect) {
-            super.init(frame: frame)
-            commonInit()
-        }
+        super.init(frame: frame)
+        commonInit()
+        setupShakeyBell()
         
-        required init?(coder aDecoder: NSCoder) {
-            super.init(coder: aDecoder)
-            commonInit()
-        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        return CGSize(width: UIView.noIntrinsicMetric, height: 144)
+    }
+    
+    private func commonInit() {
+        let bundle = Bundle(for: AccountSummaryHeaderView.self)
+        bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
+        addSubview(contentView)
+        contentView.backgroundColor = appColor
         
-        override var intrinsicContentSize: CGSize {
-            return CGSize(width: UIView.noIntrinsicMetric, height: 144)
-        }
+        contentView.translatesAutoresizingMaskIntoConstraints = false
+        contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+        contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+        contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
+    }
+    
+    private func setupShakeyBell() {
+        addSubview(ShakeyBell)
+        ShakeyBell.translatesAutoresizingMaskIntoConstraints = false
         
-        private func commonInit() {
-            let bundle = Bundle(for: AccountSummaryHeaderView.self)
-            bundle.loadNibNamed("AccountSummaryHeaderView", owner: self, options: nil)
-            addSubview(contentView)
-            contentView.backgroundColor = appColor
-            
-            contentView.translatesAutoresizingMaskIntoConstraints = false
-            contentView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-            contentView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-            contentView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-            contentView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        }
+        NSLayoutConstraint.activate([
+            bottomAnchor.constraint(equalToSystemSpacingBelow: ShakeyBell.bottomAnchor, multiplier: 0.5),
+            trailingAnchor.constraint(equalTo: ShakeyBell.trailingAnchor, constant: 0.5),
+            ShakeyBell.widthAnchor.constraint(equalToConstant: 24),
+            ShakeyBell.heightAnchor.constraint(equalToConstant: 24),
+        ])
+        
+    }
     
 }
